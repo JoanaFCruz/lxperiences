6d25695294c8b9add265a359fd188ac7ab63ac27
Joana Cruz <joana.if.cruz@gmail.com>
2022-08-22 16:42:49 +0100

    added dependent: :destroy to Experience Model

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,6 @@
 class Experience < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 
   validates :title, :description, :location, :price, :start_date, :end_date, presence: true
   validates :title, uniqueness: true
