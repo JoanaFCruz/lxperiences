c8d12a41346778a8e1b927e41939db2943634516
Joana Cruz <joana.if.cruz@gmail.com>
2022-08-23 08:58:54 +0100

    added references column in migration file and foreign_key constraint on model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
-  belongs_to :user, :experience
+  belongs_to :user, foreign_key: true
+  belongs_to :experience
   validates :start_date, comparison: { less_than: :end_date }
 end
--- db/migrate/20220822172659_create_bookings.rb
+++ db/migrate/20220822172659_create_bookings.rb
@@ -3,8 +3,8 @@ class CreateBookings < ActiveRecord::Migration[7.0]
     create_table :bookings do |t|
       t.datetime :start_date
       t.datetime :end_date
-      t.integer :user_id
-      t.integer :experience_id
+      t.references :user
+      t.references :experience
 
       t.timestamps
     end
