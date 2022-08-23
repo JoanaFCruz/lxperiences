e25c5793a922c83312e52ce52a33d1f7205944f4
Flavia Assumpcao <flaviassumpcao1@gmail.com>
2022-08-23 16:15:52 +0100

    authentication setup done

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -3,27 +3,39 @@ class ExperiencesController < ApplicationController
 
   def index
     @experiences = Experience.all
+    @experiences = policy_scope(Experience)
   end
 
   def show
+    authorize @experience
   end
 
   def new
+    @experience = Experience.new
+    authorize @experience
   end
 
   def create
-
+    @experience = Experience.new(experience_params)
+    @experience.user = current_user
+    authorize @experience
+    if @experience.save
+      redirect_to @experience, notice: "Experience was created successfully"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    authorize @experience
   end
 
   def update
-
+    authorize @experience
   end
 
   def destroy
-
+    authorize @experience
   end
 
   private
