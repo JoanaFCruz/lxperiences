8b2d9ce0c1298ac43f68bdce4229921d1620e653
Joana Cruz <joana.if.cruz@gmail.com>
2022-08-23 15:11:50 +0100

    created bookings routes and controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,32 @@
+class BookingsController < ApplicationController
+  before_action :set_experience, only: [:new, :create]
+  before_action :set_booking, only: [:show]
+
+  def index
+  end
+
+  def show
+
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def set_experience
+    @experience = Experince.find(params[:experience_id])
+  end
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+end
