dee047b15bb647166d1ca7cd147c67d83f002665
Joana Cruz <joana.if.cruz@gmail.com>
2022-08-23 16:42:39 +0100

    created relevant CRUD methods for bookings and respective views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,21 +3,30 @@ class BookingsController < ApplicationController
   before_action :set_booking, only: [:show, :destroy]
 
   def index
+    @bookings = Booking.all
   end
 
   def show
-
   end
 
   def new
+    @booking = Booking.new
   end
 
   def create
-
+    @booking = Booking.new(booking_params)
+    @booking.experience = @experience
+    @booking.user = current_user
+     if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+     end
   end
 
   def destroy
-
+    @booking.delete
+    redirect_to bookings_path, status: :see_other
   end
 
   private
