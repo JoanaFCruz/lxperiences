73fa2a9561ca85748c06f6676c2038748fef078f
Joana Cruz <joana.if.cruz@gmail.com>
2022-08-26 15:46:56 +0100

    edited buttons and footer

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -2,7 +2,7 @@ class Experience < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
-  validates :title, :description, :location, :price, presence: true
+  validates :title, :description, :location, :price, :photo, presence: true
   validates :title, uniqueness: true
 
   geocoded_by :location
